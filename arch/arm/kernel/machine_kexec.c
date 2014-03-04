ad17399687d496a5b3c74e7405c597588c21d8f8
BOOTMGR <harsh.dude16@gmail.com>
2014-03-04 13:39:34 +0530

    We're on LK 3.0.x

--- arch/arm/kernel/machine_kexec.c
+++ arch/arm/kernel/machine_kexec.c
@@ -10,7 +10,6 @@
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
 #include <asm/mmu_context.h>
-#include <asm/mmu_writeable.h>
 #include <asm/cacheflush.h>
 #include <asm/mach-types.h>
 
@@ -103,12 +102,12 @@ void machine_kexec(struct kimage *image)
 	reboot_code_buffer = page_address(image->control_code_page);
 
 	/* Prepare parameters for reboot_code_buffer*/
-	mem_text_write_kernel_word(&kexec_start_address, image->start);
-	mem_text_write_kernel_word(&kexec_indirection_page, page_list);
-	mem_text_write_kernel_word(&kexec_mach_type, machine_arch_type);
-	mem_text_write_kernel_word(&kexec_boot_atags, image->start - KEXEC_ARM_ZIMAGE_OFFSET + KEXEC_ARM_ATAGS_OFFSET);
+	kexec_start_address = image->start;
+	kexec_indirection_page = page_list;
+	kexec_mach_type = machine_arch_type;
+	kexec_boot_atags = image->start - KEXEC_ARM_ZIMAGE_OFFSET + KEXEC_ARM_ATAGS_OFFSET;
 #ifdef CONFIG_KEXEC_HARDBOOT
-	mem_text_write_kernel_word(&kexec_hardboot, image->hardboot);
+	kexec_hardboot = image->hardboot;
 #endif
 
 	/* copy our kernel relocation code to the control code page */
